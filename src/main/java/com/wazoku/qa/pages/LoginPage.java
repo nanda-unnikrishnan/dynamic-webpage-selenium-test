afe34961db87827cf885d3ddb1c56dbfbb280caa
nanda-unnikrishnan <97972506+nanda-unnikrishnan@users.noreply.github.com>
2022-02-02 02:41:35 +0000

    Added fix for modal disappearance check in Login page

--- src/main/java/com/wazoku/qa/pages/LoginPage.java
+++ src/main/java/com/wazoku/qa/pages/LoginPage.java
@@ -1,14 +1,19 @@
 package com.wazoku.qa.pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.wazoku.qa.base.PageBase;
 
 public class LoginPage extends PageBase {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginPage.class);
 
 	@FindBy(id = "emailField")
 	private WebElement email;
@@ -21,6 +26,9 @@ public class LoginPage extends PageBase {
 
 	@FindBy(xpath = "//div[@ng-messages='vm.loginForm.$error']/span[@ng-message]")
 	private WebElement errorMessageBlock;
+
+	@FindBy(xpath = "//div[@class='modal']")
+	private WebElement modalBlock;
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
@@ -37,7 +45,28 @@ public class LoginPage extends PageBase {
 		password.sendKeys(passwordInput);
 		loginButton.click();
 
+		if (!hasModalDisappeared()) {
+			LOGGER.warn("Login unsuccessful.");
+			return null;
+		}
+
 		return new HomePage(getDriver());
+
+	}
+
+	private boolean hasModalDisappeared() {
+		// Reduce timeout so as to fail fast when element is not found
+		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+		try {
+			WebDriverWait wait = new WebDriverWait(getDriver(), 1);
+			wait.until(ExpectedConditions.invisibilityOf(modalBlock));
+			return !modalBlock.isDisplayed();
+		} catch (NoSuchElementException | TimeoutException e) {
+			// Ignore this error as the modal window is not seen on successfull login
+		}
+		// Reset timeout
+		getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		return true;
 
 	}
 
